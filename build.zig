7d8b48870c5f3723fd42fdb2a216d51d24f14750
Tesseract22 <caleb2222@qq.com>
2024-03-21 14:31:57 -0500

    add .c and .h

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     // const target = b.standardTargetOptions(.{});
     // const optimize = b.standardOptimizeOption(.{});
-
-    _ = b.addModule("lodepng", .{ .root_source_file = .{ .path = "lodepng.c" }, .link_libc = true, .link_libcpp = true });
+    const mod = b.addModule("lodepng", .{ .root_source_file = .{ .path = "c.zig" }, .link_libc = true, .link_libcpp = true });
+    mod.addIncludePath(.{ .path = "." });
+    mod.addCSourceFile(.{ .file = .{ .path = "lodepng.c" } });
 }
